a65a5f1db84648350f40d039f0da5d7f7a4d3618
alifianttt <muhammad.alifianto@students.amikom.ac.id>
2020-10-08 19:43:15 +0700

    create detail ui

---
+++ app/src/main/java/com/amikomcenter/amikomcenter/DetailCategory.kt
@@ -0,0 +1,11 @@
+package com.amikomcenter.amikomcenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailCategory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_category)
+    }
+}
