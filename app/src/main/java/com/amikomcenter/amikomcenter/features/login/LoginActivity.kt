c7bed7b7543334a608beb4022a3fed8b764ebba8
zzulfiqor <zzlfiqor@gmail.com>
2020-10-17 19:40:29 +0700

    register xml

--- app/src/main/java/com/amikomcenter/amikomcenter/features/login/LoginActivity.kt
+++ app/src/main/java/com/amikomcenter/amikomcenter/features/login/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.amikomcenter.amikomcenter.features.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.amikomcenter.amikomcenter.R
+import com.amikomcenter.amikomcenter.features.register.RegisterActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -10,6 +13,10 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        btn_masuk.setOnClickListener {
+            val i = Intent(applicationContext, RegisterActivity::class.java)
+            startActivity(i)
+        }
 
     }
 }
